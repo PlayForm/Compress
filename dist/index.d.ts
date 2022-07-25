aae2a56e4f9a457d8e0370c5ca4754040ce15345
Nikola Hristov <nikola@playform.cloud>
2022-07-25 20:10:31 +0300

    0.0.14

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,7 +1,7 @@
 import type { AstroIntegration } from "astro";
 import Options from "./options";
 /**
- * It takes in an object of options, and returns an object with a name and a hook
+ * It takes in an object of options, and returns an object that Astro can use to create a plugin
  * @param {Options} integrationOptions - Options = {}
  * @returns A function that returns an object.
  */

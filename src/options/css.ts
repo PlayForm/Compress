5bf8ef2122091d40884acfcd0ac9a5d973691b88
Nikola Hristov <nikola@lightrix.dev>
2022-09-23 18:21:14 +0300

    Cleanup

--- src/options/css.ts
+++ src/options/css.ts
@@ -1,33 +1,5 @@
-import type {
-	AfterCompressFn,
-	BeforeCompressFn,
-	CompressOptions,
-	MinifyOptions,
-	Usage,
-} from "csso";
+import type { CompressOptions, MinifyOptions } from "csso";
 
 export default interface CSS extends MinifyOptions, CompressOptions {
 	[key: string]: any;
-
-	sourceMap?: boolean | undefined;
-
-	filename?: string | undefined;
-
-	debug?: boolean | undefined;
-
-	beforeCompress?: BeforeCompressFn | BeforeCompressFn[] | undefined;
-
-	afterCompress?: AfterCompressFn | AfterCompressFn[] | undefined;
-
-	restructure?: boolean;
-
-	forceMediaMerge?: boolean;
-
-	clone?: boolean;
-
-	comments?: string | boolean | undefined;
-
-	usage?: Usage | undefined;
-
-	logger?: () => {} | null;
 }
--- src/options/svg.ts
+++ src/options/svg.ts
@@ -1,13 +1,5 @@
-import type { Js2SvgOptions, OptimizeOptions } from "svgo";
+import type { OptimizeOptions } from "svgo";
 
 export default interface SVG extends OptimizeOptions {
 	[key: string]: any;
-
-	multipass?: boolean;
-
-	datauri?: "enc" | "unenc" | "base64" | undefined;
-
-	js2svg?: Js2SvgOptions;
-
-	plugins?: any;
 }

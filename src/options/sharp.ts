bbf2e0e8b3439f90debce01825148fc93618708b
Nikola Hristov <nikola@lightrix.dev>
2022-09-23 18:21:14 +0300

    Cleanup

--- src/options/sharp.ts
+++ src/options/sharp.ts
@@ -1,141 +1,19 @@
+import type sharp from "sharp";
+
 export default interface SHARP {
 	[key: string]: any;
 
-	avif?: {
-		quality?: number;
+	avif?: sharp.AvifOptions;
 
-		lossless?: boolean;
+	gif?: sharp.GifOptions;
 
-		effort?: number;
+	heif?: sharp.HeifOptions;
 
-		chromaSubsampling?: string;
-	};
+	jpeg?: sharp.JpegOptions;
 
-	gif?: {
-		colours?: number;
+	png?: sharp.PngOptions;
 
-		colors?: number;
+	tiff?: sharp.TiffOptions;
 
-		effort?: number;
-
-		dither?: number;
-
-		loop?: number;
-
-		delay?: number | Array<number>;
-
-		force?: boolean;
-	};
-
-	heif?: {
-		quality?: number;
-
-		compression?: string;
-
-		lossless?: boolean;
-
-		effort?: number;
-
-		chromaSubsampling?: string;
-	};
-
-	jpeg?: {
-		quality?: number;
-
-		progressive?: boolean;
-
-		chromaSubsampling?: string;
-
-		optimiseCoding?: boolean;
-
-		optimizeCoding?: boolean;
-
-		mozjpeg?: boolean;
-
-		trellisQuantisation?: boolean;
-
-		overshootDeringing?: boolean;
-
-		optimiseScans?: boolean;
-
-		optimizeScans?: boolean;
-
-		quantisationTable?: number;
-
-		quantizationTable?: number;
-
-		force?: boolean;
-	};
-
-	png?: {
-		progressive?: boolean;
-
-		compressionLevel?: number;
-
-		adaptiveFiltering?: boolean;
-
-		palette?: boolean;
-
-		quality?: number;
-
-		effort?: number;
-
-		colours?: number;
-
-		colors?: number;
-
-		dither?: number;
-
-		force?: boolean;
-	};
-
-	raw?: {
-		depth?: string;
-	};
-
-	tiff?: {
-		quality?: number;
-
-		force?: boolean;
-
-		compression?: string;
-
-		predictor?: string;
-
-		pyramid?: boolean;
-
-		tile?: boolean;
-
-		tileWidth?: number;
-
-		tileHeight?: number;
-
-		xres?: number;
-
-		yres?: number;
-
-		resolutionUnit?: string;
-
-		bitdepth?: number;
-	};
-
-	webp?: {
-		quality?: number;
-
-		alphaQuality?: number;
-
-		lossless?: boolean;
-
-		nearLossless?: boolean;
-
-		smartSubsample?: boolean;
-
-		effort?: number;
-
-		loop?: number;
-
-		delay?: number | Array<number>;
-
-		force?: boolean;
-	};
+	webp?: sharp.WebpOptions;
 }
--- src/options/squoosh.ts
+++
@@ -1,3 +0,0 @@
-export default interface SQUOOSH {
-	[key: string]: any;
-}

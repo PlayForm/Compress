a25b3ea8ee300d41be9905033188177f80a036dc
Nikola Hristov <nikola@lightrix.dev>
2022-09-24 11:22:23 +0300

    Cleanup

--- dist/options/css.d.ts
+++ dist/options/css.d.ts
@@ -1,15 +1,4 @@
-import type { AfterCompressFn, BeforeCompressFn, CompressOptions, MinifyOptions, Usage } from "csso";
+import type { CompressOptions, MinifyOptions } from "csso";
 export default interface CSS extends MinifyOptions, CompressOptions {
     [key: string]: any;
-    sourceMap?: boolean | undefined;
-    filename?: string | undefined;
-    debug?: boolean | undefined;
-    beforeCompress?: BeforeCompressFn | BeforeCompressFn[] | undefined;
-    afterCompress?: AfterCompressFn | AfterCompressFn[] | undefined;
-    restructure?: boolean;
-    forceMediaMerge?: boolean;
-    clone?: boolean;
-    comments?: string | boolean | undefined;
-    usage?: Usage | undefined;
-    logger?: () => {} | null;
 }
--- dist/options/sharp.d.ts
+++ dist/options/sharp.d.ts
@@ -1,80 +1,11 @@
+import type sharp from "sharp";
 export default interface SHARP {
     [key: string]: any;
-    avif?: {
-        quality?: number;
-        lossless?: boolean;
-        effort?: number;
-        chromaSubsampling?: string;
-    };
-    gif?: {
-        colours?: number;
-        colors?: number;
-        effort?: number;
-        dither?: number;
-        loop?: number;
-        delay?: number | Array<number>;
-        force?: boolean;
-    };
-    heif?: {
-        quality?: number;
-        compression?: string;
-        lossless?: boolean;
-        effort?: number;
-        chromaSubsampling?: string;
-    };
-    jpeg?: {
-        quality?: number;
-        progressive?: boolean;
-        chromaSubsampling?: string;
-        optimiseCoding?: boolean;
-        optimizeCoding?: boolean;
-        mozjpeg?: boolean;
-        trellisQuantisation?: boolean;
-        overshootDeringing?: boolean;
-        optimiseScans?: boolean;
-        optimizeScans?: boolean;
-        quantisationTable?: number;
-        quantizationTable?: number;
-        force?: boolean;
-    };
-    png?: {
-        progressive?: boolean;
-        compressionLevel?: number;
-        adaptiveFiltering?: boolean;
-        palette?: boolean;
-        quality?: number;
-        effort?: number;
-        colours?: number;
-        colors?: number;
-        dither?: number;
-        force?: boolean;
-    };
-    raw?: {
-        depth?: string;
-    };
-    tiff?: {
-        quality?: number;
-        force?: boolean;
-        compression?: string;
-        predictor?: string;
-        pyramid?: boolean;
-        tile?: boolean;
-        tileWidth?: number;
-        tileHeight?: number;
-        xres?: number;
-        yres?: number;
-        resolutionUnit?: string;
-        bitdepth?: number;
-    };
-    webp?: {
-        quality?: number;
-        alphaQuality?: number;
-        lossless?: boolean;
-        nearLossless?: boolean;
-        smartSubsample?: boolean;
-        effort?: number;
-        loop?: number;
-        delay?: number | Array<number>;
-        force?: boolean;
-    };
+    avif?: sharp.AvifOptions;
+    gif?: sharp.GifOptions;
+    heif?: sharp.HeifOptions;
+    jpeg?: sharp.JpegOptions;
+    png?: sharp.PngOptions;
+    tiff?: sharp.TiffOptions;
+    webp?: sharp.WebpOptions;
 }

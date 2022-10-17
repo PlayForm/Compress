7373dabb3a0494841abbac578965c8702fa420bb
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-17 18:25:49 +0300

    Cleanup

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { AstroIntegration } from "astro";
-import { Options } from "./options/index.js";
-declare const _default: (options?: Options) => AstroIntegration;
-export default _default;

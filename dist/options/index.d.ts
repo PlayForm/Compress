99afd5e6cd78c9de55cf3f8c4769c7a5986bb681
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-17 19:48:24 +0300

    Cleanup

---
+++ dist/options/index.d.ts
@@ -0,0 +1,17 @@
+import type CSS from "./css";
+import type HTML from "./html";
+import type IMG from "./img";
+import type JS from "./js";
+import type SVG from "./svg";
+export interface Options {
+    [key: string]: any;
+    path?: string;
+    css?: boolean | CSS;
+    html?: boolean | HTML;
+    js?: boolean | JS;
+    img?: boolean | IMG;
+    svg?: boolean | SVG;
+    logger?: number;
+}
+declare const _default: () => Options;
+export default _default;

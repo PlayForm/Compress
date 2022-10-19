2f6aa7856e6185c4ced90840d06316c49158ddf8
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-19 12:46:43 +0300

    Cleanup

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,1 @@
-import { deepmerge as p } from "deepmerge-ts";
-import n from "./lib/pipe-all.js";
-import e from "./options/index.js";
-var h=(r={})=>{var a;for(const o in r)Object.prototype.hasOwnProperty.call(r,o)&&r[o]===!0&&(r[o]=e()[o]);const t=p(e(),r);return t.path=(a=t.path)!=null&&a.endsWith("/")?t.path:`${t.path}/`,{name:"astro-compress",hooks:{"astro:config:done":async o=>{t.path=t.path?t.path:o.config.outDir.toString()},"astro:build:done":async()=>{await n(t,t.logger)}}}};export { h as default };
-
+import{deepmerge as p}from"deepmerge-ts";import n from"./lib/pipe-all.js";import e from"./options/index.js";var h=(r={})=>{var a;for(const o in r)Object.prototype.hasOwnProperty.call(r,o)&&r[o]===!0&&(r[o]=e()[o]);const t=p(e(),r);return t.path=(a=t.path)!=null&&a.endsWith("/")?t.path:`${t.path}/`,{name:"astro-compress",hooks:{"astro:config:done":async o=>{t.path=t.path?t.path:o.config.outDir.toString()},"astro:build:done":async()=>{await n(t,t.logger)}}}};export{h as default};

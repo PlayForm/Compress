f4fa82e0f187fd19b0929cdc14c59188a775e1e3
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-28 19:45:24 +0300

    squash!

---
+++ dist/options/index.d.ts
@@ -0,0 +1,19 @@
+import type CSS from "./css";
+import type HTML from "./html";
+import type IMG from "./img";
+import type JS from "./js";
+import type SVG from "./svg";
+export declare type filterFunction = (file: string) => boolean;
+export interface Options {
+    [key: string]: any;
+    path?: string;
+    exclude?: string | RegExp | filterFunction | [string] | [RegExp] | [filterFunction];
+    css?: boolean | CSS;
+    html?: boolean | HTML;
+    js?: boolean | JS;
+    img?: boolean | IMG;
+    svg?: boolean | SVG;
+    logger?: number;
+}
+declare const _default: () => Options;
+export default _default;

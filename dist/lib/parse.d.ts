6d85c8a88ac4ed782fc9dc1ce418b44fb59c83c3
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-28 19:45:24 +0300

    squash!

---
+++ dist/lib/parse.d.ts
@@ -0,0 +1,3 @@
+import type { Options } from "../options/index.js";
+declare const _default: (glob: string, debug: number | undefined, type: string | undefined, exclude: Options["exclude"], write?: (data: string) => any, read?: (file: string) => any) => Promise<void>;
+export default _default;

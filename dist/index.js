f01d84632f19c04daa32cd97fc848b3fb8d9394d
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-10 14:26:10 +0200

    squash!

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-import i from"@nikolarhristov/pipeline";var t=(o={})=>({name:"astro-compress",hooks:{"astro:build:done":async()=>{await new i(o).compress()}}});export{t as default};
+import{pipeline as i}from"@nikolarhristov/pipeline";var t=(o={})=>({name:"astro-compress",hooks:{"astro:build:done":async()=>{await new i(o).compress()}}});export{t as default};

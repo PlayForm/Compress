31e30fec9f0b3ebaea55d8c097cb1d31cfbfd195
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-17 13:26:28 +0200

    1.1.23

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
 import type { AstroIntegration } from "astro";
-import type { Options } from "@nikolarhristov/pipeline/dist/options/lib/compress/index.js";
+import type { Options } from "files-pipeline/dist/options/lib/compress/index.js";
 declare const _default: (options?: Options) => AstroIntegration;
 export default _default;
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import type { AstroIntegration } from "astro";
 
-import { pipeline } from "@nikolarhristov/pipeline";
-import type { Options } from "@nikolarhristov/pipeline/dist/options/lib/compress/index.js";
+import { pipeline } from "files-pipeline";
+import type { Options } from "files-pipeline/dist/options/lib/compress/index.js";
 
 export default (options: Options = {}): AstroIntegration => ({
 	name: "astro-compress",

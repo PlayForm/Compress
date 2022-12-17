df49a003e76e2900f13f77668c78a8e9fe20fca9
Nikola Hristov <nikola@nikolahristov.tech>
2022-12-17 13:26:28 +0200

    1.1.23

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,1 @@
-import{pipeline as t}from"@nikolarhristov/pipeline";var r=(o={})=>({name:"astro-compress",hooks:{"astro:build:done":async()=>{await new t(o).compress()}}});export{r as default};
+import{pipeline as t}from"files-pipeline";var e=(o={})=>({name:"astro-compress",hooks:{"astro:build:done":async()=>{await new t(o).compress()}}});export{e as default};

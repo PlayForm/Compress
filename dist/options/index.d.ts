00d334dc09be5f4dc35020028f0f1881612a5b24
Nikola Hristov <nikola@nikolahristov.tech>
2023-01-20 19:00:05 +0200

    squash!

--- dist/options/index.d.ts
+++ dist/options/index.d.ts
@@ -1,8 +1,8 @@
-import type CSS from "./css.js";
-import type HTML from "./html.js";
-import type IMG from "./img.js";
-import type JS from "./js.js";
-import type SVG from "./svg.js";
+import type { CSS } from "./css.js";
+import type { HTML } from "./html.js";
+import type { IMG } from "./img.js";
+import type { JS } from "./js.js";
+import type { SVG } from "./svg.js";
 import type { Options as OptionsBase } from "files-pipeline/dist/options/index.js";
 export interface Options extends OptionsBase {
     [key: string]: any;
--- src/options/js.ts
+++ src/options/js.ts
@@ -1,6 +1,18 @@
 import type { MinifyOptions } from "terser";
 
-export default interface JS extends MinifyOptions {
+export interface JS extends MinifyOptions {
 	// rome-ignore lint/suspicious/noExplicitAny:
 	[key: string]: any;
 }
+
+export default {
+	// rome-ignore lint/nursery/noPrecisionLoss:
+	ecma: 5,
+	enclose: false,
+	keep_classnames: false,
+	keep_fnames: false,
+	ie8: false,
+	module: false,
+	safari10: false,
+	toplevel: false,
+} satisfies JS;

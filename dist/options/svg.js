1db47ef5d10a40701e6d14a669d3e929ccbaeeb1
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-11 13:46:05 +0200

    Refactor: switch import locations for `defaultsSVG` and `SVG`

--- dist/options/svg.js
+++ dist/options/svg.js
@@ -1,1 +1,2 @@
-var e={multipass:!0,js2svg:{indent:0,pretty:!1},plugins:["preset-default"]};export{e as default};
+var e={multipass:!0,js2svg:{indent:0,pretty:!1},plugins:["preset-default"]};export { e as default };
+

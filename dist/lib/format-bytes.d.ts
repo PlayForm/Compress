ecb1074fb170d7bf3392b5094e9d50ff36971bf0
Nikola Hristov <nikola@nikolahristov.tech>
2023-03-29 22:47:43 +0300

    squash!

--- dist/lib/format-bytes.d.ts
+++ dist/lib/format-bytes.d.ts
@@ -1,2 +1,8 @@
-declare const _default: (bytes: number, decimals?: number) => Promise<string>;
-export default _default;
+/**
+ * Convert bytes to human readable format.
+ * @param {number} bytes - The number of bytes to format.
+ * @param {number} [decimals=2.0] - The number of decimal places to show.
+ * @returns A function that takes two parameters, bytes and decimals.
+ */
+declare const formatBytes: (bytes: number, decimals?: number) => Promise<string>;
+export default formatBytes;

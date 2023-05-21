5b98e11ee2b79437d47d360fc8ee31b40de67b80
Nikola Hristov <nikola@nikolahristov.tech>
2023-05-21 17:59:42 +0300

    squash!

---
+++ dist/options/map.d.ts
@@ -0,0 +1,11 @@
+export interface MAP {
+    [key: string]: string;
+}
+declare const _default: {
+    css: string;
+    html: string;
+    js: string;
+    img: string;
+    svg: string;
+};
+export default _default;

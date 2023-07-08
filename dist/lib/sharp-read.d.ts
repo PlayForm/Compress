b2f0496310911080e7c49f45f61a3393f24becbc
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-08 20:22:58 +0300

    squash!

--- dist/lib/sharp-read.d.ts
+++ dist/lib/sharp-read.d.ts
@@ -1,6 +1,6 @@
 import type { optionExecutionsFile } from "files-pipe/dist/options/index.js";
-import type { Sharp } from "sharp";
 import type { IMG } from "../options/img.js";
+import type { Sharp } from "sharp";
 export interface sharpBuffer extends Sharp {
     [key: string]: any;
 }

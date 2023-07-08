4ad7be474db454981269ec2cbc4df591c618b245
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-08 15:55:20 +0300

    squash!

--- src/options/index.ts
+++ src/options/index.ts
@@ -1,19 +1,19 @@
-import deepmerge from "files-pipe/dist/lib/deepmerge.js";
 import type { Options as OptionsBase } from "files-pipe/dist/options/index.js";
+import deepmerge from "files-pipe/dist/lib/deepmerge.js";
 import defaults from "files-pipe/dist/options/index.js";
 import formatBytes from "../lib/format-bytes.js";
-import type { CSS } from "./css.js";
-import defaultsCSS from "./css.js";
 import type { HTML } from "./html.js";
 import defaultsHTML from "./html.js";
+import type { CSS } from "./css.js";
 import type { IMG } from "./img.js";
+import type { MAP } from "./map.js";
+import type { SVG } from "./svg.js";
+import defaultsCSS from "./css.js";
 import defaultsIMG from "./img.js";
+import defaultsMAP from "./map.js";
+import defaultsSVG from "./svg.js";
 import type { JS } from "./js.js";
 import defaultsJS from "./js.js";
-import type { MAP } from "./map.js";
-import defaultsMAP from "./map.js";
-import type { SVG } from "./svg.js";
-import defaultsSVG from "./svg.js";
 
 export interface Options extends OptionsBase {
 	// rome-ignore lint/suspicious/noExplicitAny:

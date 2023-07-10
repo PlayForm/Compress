731057d50d52f205a2eecf3f421ddde0fff3b7d2
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-10 03:14:18 +0300

    squash!

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Options } from "./options/index.js";
-import type { AstroIntegration } from "astro";
-declare const _default: (options?: Options) => AstroIntegration;
-export default _default;

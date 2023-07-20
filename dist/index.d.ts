f59ed61cb6a13c8a95f19b3b9006a749703c0827
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-20 14:53:14 +0300

    squash!

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-import type { Options } from "./options/index.js";
 import type { AstroIntegration } from "astro";
+import type { Options } from "./options/index.js";
 declare const _default: (options?: Options) => AstroIntegration;
 export default _default;
--- dist/lib/sharp-read.d.ts
+++ dist/lib/sharp-read.d.ts
@@ -1,6 +1,6 @@
 import type { optionExecutionsFile } from "files-pipe/dist/options/index.js";
-import type { IMG } from "../options/img.js";
 import type { Sharp } from "sharp";
+import type { IMG } from "../options/img.js";
 export interface sharpBuffer extends Sharp {
     [key: string]: any;
 }

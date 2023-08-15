7d58f4585186e8814671f18fb72281cd2b49db14
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-15 15:00:02 +0300

    squash!

--- Source/Option/Map.ts
+++ Source/Option/Map.ts
@@ -1,4 +1,4 @@
-export interface MAP {
+export interface Map {
 	[key: string]: string;
 }
 
@@ -8,4 +8,4 @@ export default {
 	Image: "**/*.{avci,avcs,avif,avifs,gif,heic,heics,heif,heifs,jfif,jif,jpe,jpeg,jpg,apng,png,raw,tiff,webp}",
 	JavaScript: "**/*.{js,mjs,cjs,jsm}",
 	SVG: "**/*.svg",
-} satisfies MAP;
+} satisfies Map;

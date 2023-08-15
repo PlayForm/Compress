1f317dfe4903a11f5c510b2a5480a675ae52f297
Nikola Hristov <nikola@nikolahristov.tech>
2023-08-15 15:24:46 +0300

    squash!

--- Target/Option/Index.d.ts
+++ Target/Option/Index.d.ts
@@ -3,7 +3,7 @@ import type { CSS } from "./CSS.js";
 import type { HTML } from "./HTML.js";
 import type { Image } from "./Image.js";
 import type { JavaScript } from "./JavaScript.js";
-import type { MAP } from "./Map.js";
+import type { Map as _Map } from "./Map.js";
 import type { SVG } from "./SVG.js";
 export interface Option extends _Option {
     [key: string]: any;
@@ -12,7 +12,7 @@ export interface Option extends _Option {
     JavaScript?: boolean | JavaScript;
     Image?: boolean | Image;
     SVG?: boolean | SVG;
-    Map?: boolean | MAP;
+    Map?: boolean | _Map;
 }
 declare const _default: Option;
 export default _default;

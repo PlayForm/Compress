bc37a88eb825c7abc2b03798a7aa6d861ba3ea84
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-04 19:03:46 +0300

    squash!

--- Target/Index.d.ts
+++
@@ -1,4 +0,0 @@
-import type { AstroIntegration } from "astro";
-import type { Option } from "./Option/Index.js";
-declare const _default: (_Option?: Option) => AstroIntegration;
-export default _default;

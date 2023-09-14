3c8c055d2146838cb0aa4e8ab660b6f5610aa0ae
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-15 00:01:11 +0300

    squash!

--- Source/Option/Index.ts
+++ Source/Option/Index.ts
@@ -42,11 +42,17 @@ export interface Option extends _Option {
 
 export default Merge(Default, {
 	CSS: (await import("./CSS.js")).default,
+
 	HTML: (await import("./HTML.js")).default,
+
 	JavaScript: (await import("./JavaScript.js")).default,
+
 	Image: (await import("./Image.js")).default,
+
 	SVG: (await import("./SVG.js")).default,
+	
 	Map: (await import("./Map.js")).default,
+
 	Action: {
 		Failed: async (On) => `Error: Cannot compress file ${On.Input}!`,
 		Passed: async (On) =>

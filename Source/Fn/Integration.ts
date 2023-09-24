bc9ae548265643333be8f347975add26cf1c11ec
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-24 16:24:12 +0300

    squash!

--- Source/Fn/Integration.ts
+++ Source/Fn/Integration.ts
@@ -1,12 +1,12 @@
-import type On from "./Interface/Image/On.js";
-import type Option from "./Interface/Option.js";
+import type On from "../Interface/Image/On.js";
+import type Option from "../Interface/Option.js";
 
 import type Action from "files-pipe/Target/Interface/Action.js";
 import type Path from "files-pipe/Target/Interface/Path.js";
 
 import type { AstroIntegration } from "astro";
 
-export const { default: Default } = await import("./Object/Option.js");
+export const { default: Default } = await import("../Object/Option.js");
 
 export const { default: _Default } = await import(
 	"files-pipe/Target/Object/Option.js"

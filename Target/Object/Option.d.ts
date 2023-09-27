e3b630d6aabd4d9a4203a1581166da7accdd97de
Nikola Hristov <nikola@nikolahristov.tech>
2023-09-27 16:50:26 +0300

    squash!

--- Target/Object/Option.d.ts
+++ Target/Object/Option.d.ts
@@ -91,14 +91,16 @@ declare const _default: Omit<{} & {
         Folder: string;
     };
     Path: string;
+    Exclude: false;
+    Files: string;
     Action: Omit<{} & {
+        Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<boolean>;
+        Accomplished: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
+        Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<any>;
         Read: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Wrote: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Interface/Buffer.js").Type>;
-        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<boolean>;
-        Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Accomplished: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
-        Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<any>;
     }, "__proto__">;
     Logger: 2;
 }, "__proto__">;

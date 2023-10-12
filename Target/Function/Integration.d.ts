0597e70351f75bb804b73d54a1437d675c551923
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-12 11:58:04 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -112,25 +112,6 @@ export declare const Default: Omit<{} & {
     }, "__proto__">;
     Logger: 2;
 }, "__proto__">;
-export declare const _Default: {
-    Cache: {
-        Search: string;
-        Folder: string;
-    };
-    Path: string;
-    Logger: 2;
-    Action: {
-        Read: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Interface/Buffer.js").Type>;
-        Passed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<boolean>;
-        Failed: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Accomplished: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Fulfilled: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
-        Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<import("files-pipe/Target/Interface/Plan.js").default>;
-    };
-    Files: string;
-    Exclude: false;
-};
+export declare const Search: string;
 export declare const Merge: import("typescript-esbuild/Target/Interface/Merge.js").default<import("typescript-esbuild/Target/Interface/Merge.js").Generic>;
 export declare const sharp: typeof import("sharp");
-export declare const Files: typeof import("files-pipe").default;

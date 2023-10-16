9185b5f9dba4815c18fe583d153114ad3713604a
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-16 15:25:18 +0300

    squash!

---
+++ Target/Function/Image.d.ts
@@ -0,0 +1,21 @@
+/**
+ * @module Image
+ *
+ */
+declare const _default: (Option: Option, { Buffer, Input }: On) => Promise<any>;
+export default _default;
+import type On from "../Interface/Image/On.js";
+import type Option from "../Interface/Image/Option.js";
+export declare const _Map: {
+    avci: string;
+    avcs: string;
+    avifs: string;
+    heic: string;
+    heics: string;
+    heifs: string;
+    jfif: string;
+    jif: string;
+    jpe: string;
+    apng: string;
+    jpg: string;
+};

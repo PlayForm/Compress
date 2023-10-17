824f67995a2c5d2d7a137990f478dbfb3d4c613a
Nikola Hristov <nikola@nikolahristov.tech>
2023-10-17 21:19:31 +0300

    squash!

--- Target/Function/Integration.d.ts
+++ Target/Function/Integration.d.ts
@@ -1,3 +1,4 @@
+/// <reference types="node" />
 /**
  * @module Integration
  *
@@ -108,7 +109,7 @@ export declare const Default: Omit<{} & {
         Accomplished: (On: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
         Changed: (Plan: import("files-pipe/Target/Interface/Plan.js").default) => Promise<any>;
         Read: ({ Input }: import("files-pipe/Target/Interface/File.js").default) => Promise<string>;
-        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<import("files-pipe/Target/Interface/Buffer.js").Type>;
+        Wrote: ({ Buffer }: import("files-pipe/Target/Interface/File.js").default) => Promise<Buffer>;
         Fulfilled: ({ Files }: import("files-pipe/Target/Interface/Plan.js").default) => Promise<string | false>;
     }, "__proto__">;
     Logger: 2;

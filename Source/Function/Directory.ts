1d0a2748d42fa80aff0c075331a6f6999f093ee4
Nikola Hristov <nikola@nikolahristov.tech>
2024-01-25 23:30:32 +0200

    squash!

--- Source/Function/Directory.ts
+++ Source/Function/Directory.ts
@@ -4,9 +4,9 @@
  *
  */
 export default (async (...[Path]: Parameters<Type>) => {
-	let Clean = (await import("node:path")).parse(Path).dir;
+	let Clean = (await import("path")).parse(Path).dir;
 
-	Clean = (await import("node:path")).normalize(Clean);
+	Clean = (await import("path")).normalize(Clean);
 	Clean = Clean.replace(/\\/g, "/");
 	Clean = Clean.replace(
 		(await import("../Function/Integration.js")).System,

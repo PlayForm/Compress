a6c43cfe86cb49196a2e64c62fb00c9446ec7e58
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-02 16:32:39 +0300

    squash!

--- Source/Function/Integration.ts
+++ Source/Function/Integration.ts
@@ -47,7 +47,7 @@ export default ((...[_Option = {}]: Parameters<Type>) => {
 	}
 
 	return {
-		name: "@playform/compress-astro",
+		name: "@playform/compress",
 		hooks: {
 			"astro:config:done": async ({
 				config: {

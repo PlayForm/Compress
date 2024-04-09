76e984d86d47efc92850ce6df2e9e17b61e1b57b
Nikola R. Hristov <nikola@nikolahristov.tech>
2024-04-09 21:19:14 +0300

    squash!

--- Source/Type/Image/Filesharp.ts
+++ Source/Type/Image/Filesharp.ts
@@ -6,6 +6,4 @@ export type Type = keyof typeof _Map;
 
 export type { Type as default };
 
-export const { default: _Map } = await import(
-	"@Variable/Image/Mapsharp.js"
-);
+export const { default: _Map } = await import("@Variable/Image/Mapsharp.js");

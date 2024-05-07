1a24d98bb1a0e819eb050b0707de765871cb36d1
Nikola Hristov <nikola@playform.cloud>
2024-05-07 03:28:49 +0300

    squash!

--- Source/Variable/Map.ts
+++ Source/Variable/Map.ts
@@ -6,7 +6,7 @@ export default {
 	CSS: "**/*.css",
 	HTML: "**/*.html",
 	Image: "**/*.{apng,avci,avcs,avif,avifs,gif,heic,heics,heif,heifs,jfif,jif,jpe,jpeg,jpg,png,raw,tiff,webp}",
-	JavaScript: "**/*.{js,mjs,cjs,jsm}",
+	JavaScript: "**/*.{js,mjs,cjs,jsm,mts,tsx,ts}",
 	SVG: "**/*.svg",
 } satisfies Interface;
 

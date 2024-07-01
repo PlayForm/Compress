5b23547e3cf572761a9b8bd271e5467f48fabc1b
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-01 16:52:05 +0300

    squash!
    Signed-off-by: Nikola Hristov <Nikola@PlayForm.Cloud>

---
+++ Target/Variable/Map.js
@@ -0,0 +1,1 @@
+var e={CSS:"**/*.css",HTML:"**/*.html",Image:"**/*.{apng,avci,avcs,avif,avifs,gif,heic,heics,heif,heifs,jfif,jif,jpe,jpeg,jpg,png,raw,tiff,webp}",JavaScript:"**/*.{js,mjs,cjs,jsm,mts,tsx,ts}",SVG:"**/*.svg"};export{e as default};

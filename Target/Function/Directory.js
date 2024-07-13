278ddbeb5485a76f35f86a89871f4c2bbeda857b
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-07-14 00:59:16 +0300

    squash!

--- Target/Function/Directory.js
+++ Target/Function/Directory.js
@@ -1,1 +1,1 @@
-var t=async(...[r])=>{let e=(await import("node:path")).normalize((await import("node:path")).parse(r).dir).replace(/\\/g,"/").replace((await import("./Integration.js")).System,"");return e.endsWith("/")||(e+="/"),e};export{t as default};
+var t=async(...[r])=>{let e=(await import("path")).normalize((await import("path")).parse(r).dir).replace(/\\/g,"/").replace((await import("./Integration.js")).System,"");return e.endsWith("/")||(e+="/"),e};export{t as default};
